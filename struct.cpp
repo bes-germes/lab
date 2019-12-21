a052ebd63a2a08b95b58071111401e01aa66faeb
bes-germes <55259601+bes-germes@users.noreply.github.com>
2019-12-21 16:28:18 +0300

    Update struct.cpp

--- struct.cpp
+++ struct.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 typedef struct
 {
-    char x; /*1 bytes*/
-    short y; /*2 bytes*/
-    long z; /*8 bytes*/
+    short x ;
+    long y;
+    double z;
 }Ordered;
 
 typedef struct
 {
-    short y; /*2 bytes*/
-    long z; /*8 bytes*/
-    char x; /*1 bytes*/
+    long y;
+    double z;
+    short x ;
 }Disordered;
 
 int main()
@@ -22,16 +22,19 @@ int main()
     Disordered *disordered = new Disordered;
 
 
-    cout << "Size of char: " << sizeof(ordered->x) << endl;
-    cout << "Size of short: " << sizeof(ordered->y) << endl;
-    cout << "Size of long: " << sizeof(ordered->z) << endl;
+    cout << "Size of short: " << sizeof(ordered->x) << endl;
+    cout << "Size of long: " << sizeof(ordered->y) << endl;
+    cout << "Size of double: " << sizeof(ordered->z) << endl;
     cout << "Size of Ordered: " << sizeof(Ordered) << endl;
-    cout << "Adress of of char in ordered: " << &(ordered->x) << endl;  
-    cout << "Adress of of short in ordered: " << &(ordered->y) << endl;
-    cout << "Adress of of long in ordered: " << &(ordered->z) << endl;
     cout << "Size of Disordered: " << sizeof(Disordered) << endl;
+    cout << "Adres of of short in ordered: " << &(ordered->x) << endl;
+    cout << "Adress of of long in ordered: " << &(ordered->y) << endl;
+    cout << "Adress of of double in ordered: " << &(ordered->z) << endl;
+    cout << "Adres of of short in disordered: " << &(disordered->x) << endl;
+    cout << "Adress of of long in disordered: " << &(disordered->y) << endl;
+    cout << "Adress of of double in disordered: " << &(disordered->z) << endl;
     cout << "Sum of fields in Ordered struct: " << sizeof(ordered->x) + sizeof(ordered->y) + sizeof(ordered->z) << endl;
     cout << "Sum of fields in Disordered struct: " << sizeof(disordered->x) + sizeof(disordered->y) + sizeof(disordered->z) << endl;
-    
+
     return 0;
 }
