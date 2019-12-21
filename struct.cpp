33dcd9d1c144c8034fbf1c3fca33efa47ee13aab
bes-germes <55259601+bes-germes@users.noreply.github.com>
2019-12-21 16:12:51 +0300

    Update struct.cpp

--- struct.cpp
+++ struct.cpp
@@ -26,9 +26,9 @@ int main()
     cout << "Size of short: " << sizeof(ordered->y) << endl;
     cout << "Size of long: " << sizeof(ordered->z) << endl;
     cout << "Size of Ordered: " << sizeof(Ordered) << endl;
-    cout << "Adress of of char in ordered" << &(ordered->x) << endl;  
-    cout << "Adress of of short in ordered" << &(ordered->y) << endl;
-    cout << "Adress of of long in ordered" << &(ordered->z) << endl;
+    cout << "Adress of of char in ordered: " << &(ordered->x) << endl;  
+    cout << "Adress of of short in ordered: " << &(ordered->y) << endl;
+    cout << "Adress of of long in ordered: " << &(ordered->z) << endl;
     cout << "Size of Disordered: " << sizeof(Disordered) << endl;
     cout << "Sum of fields in Ordered struct: " << sizeof(ordered->x) + sizeof(ordered->y) + sizeof(ordered->z) << endl;
     cout << "Sum of fields in Disordered struct: " << sizeof(disordered->x) + sizeof(disordered->y) + sizeof(disordered->z) << endl;
