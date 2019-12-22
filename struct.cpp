82558025b62866fc4c09a0d0c56e8c7c0094659d
bes-germes <55259601+bes-germes@users.noreply.github.com>
2019-12-22 16:55:01 +0300

    Update struct.cpp

--- struct.cpp
+++ struct.cpp
@@ -4,37 +4,64 @@ using namespace std;
 
 typedef struct
 {
-    short x ;
-    long y;
-    double z;
-}Ordered;
+    bool Home;
+    int Queue;
+    char Name[20];
+    float Age;
+    bool Alright;
+    char SecondName[20];
+}Client;
+
+#pragma pack(push, 1)
+typedef struct
+{
+    bool Home;
+    int Queue;
+    char Name[20];
+    float Age;
+    bool Alright;
+    char SecondName[20];
+}Client1;
+#pragma pack(pop)
 
 typedef struct
 {
-    long y;
-    double z;
-    short x ;
-}Disordered;
+    bool Home;
+    bool Alright;
+    char Name[20];
+    char SecondName[20];
+    int Queue;
+    float Age;
+}DisorderedClient;
+
+#pragma pack(push, 1)
+typedef struct
+{
+    bool Home;
+    bool Alright;
+    char Name[20];
+    char SecondName[20];
+    int Queue;
+    float Age;
+}DisorderedClient1;
+#pragma pack(pop)
 
 int main()
 {
-    Ordered *ordered = new Ordered;
-    Disordered *disordered = new Disordered;
-
-
-    cout << "Size of short: " << sizeof(ordered->x) << endl;
-    cout << "Size of long: " << sizeof(ordered->y) << endl;
-    cout << "Size of double: " << sizeof(ordered->z) << endl;
-    cout << "Size of Ordered: " << sizeof(Ordered) << endl;
-    cout << "Size of Disordered: " << sizeof(Disordered) << endl;
-    cout << "Adres of of short in ordered: " << &(ordered->x) << endl;
-    cout << "Adress of of long in ordered: " << &(ordered->y) << endl;
-    cout << "Adress of of double in ordered: " << &(ordered->z) << endl;
-    cout << "Adres of of short in disordered: " << &(disordered->x) << endl;
-    cout << "Adress of of long in disordered: " << &(disordered->y) << endl;
-    cout << "Adress of of double in disordered: " << &(disordered->z) << endl;
-    cout << "Sum of fields in Ordered struct: " << sizeof(ordered->x) + sizeof(ordered->y) + sizeof(ordered->z) << endl;
-    cout << "Sum of fields in Disordered struct: " << sizeof(disordered->x) + sizeof(disordered->y) + sizeof(disordered->z) << endl;
-
+    Client client;
+    Client1 client1;
+    DisorderedClient disorderedClient;
+    DisorderedClient1 disorderedClient1;
+    cout << "Size of bool; " << sizeof(client.Alright) << endl;
+    cout << "Size of char: " << sizeof(client.Name) << endl;
+    cout << "Size of int: " << sizeof(client.Queue) << endl;
+    cout << "Size of float: " << sizeof(client.Age) << endl;
+    cout << "Size of Client: " << sizeof(disorderedClient) << endl;
+    cout << "Size of Client1: " << sizeof(disorderedClient1) << endl;
+    cout << "Size of DisorderedClient: " << sizeof(Client) << endl;
+    cout << "Size of DisorderedClient1: " << sizeof(Client1) << endl;
+    cout << "Sum of fields in Client struct: " << sizeof(client.Age) + sizeof(client.Queue) + sizeof(client.Name) + sizeof(client.Alright) << endl;
+    cout << "Sum of fields in DisorderedClient struct: " <<  sizeof(disorderedClient.Age) + sizeof(disorderedClient.Queue) + sizeof(disorderedClient.Name) + sizeof(disorderedClient.Alright) << endl;
+    cout << "Sum of structs: " << sizeof(Client) + sizeof(disorderedClient) + sizeof(disorderedClient1)<< endl;
     return 0;
 }
