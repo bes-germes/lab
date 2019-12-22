6f8215f794afef772b21ee55f4a766b123c0c8dd
bes-germes <55259601+bes-germes@users.noreply.github.com>
2019-12-22 16:54:16 +0300

    Update variable.cpp

--- variable.cpp
+++ variable.cpp
@@ -2,27 +2,26 @@
 
 using namespace std;
 
-int global_variable_1 = 0;
-int global_variable_2 = 0;
+int global_variable_1;
+int global_variable_2;
 
 int main()
 {
-    int local_variable_l = 0;
-    int local_variable_2 = 0;
+    int local_variable_l;
+    int local_variable_2;
 
-    int *single_int_1 = new int[1];
-    int *single_int_2 = new int[1];
+    int *single_int_1 = new int;
+    int *single_int_2 = new int;
 
     cout << "&global_variable_1 = " << &global_variable_1 << "; " << endl;
-    cout << "&global_variable_2 =" << &global_variable_2 << ";" << endl;
+    cout << "&global_variable_2 = " << &global_variable_2 << ";" << endl;
     cout << "&local_variable_l = " << &local_variable_l << ";" << endl;
     cout << "&local_variable_2 = " << &local_variable_2 << ";" << endl;
-    cout << "&single_int_1 = " << &single_int_1[0] << "; " << endl;
-    cout << "&single_int_2 = " << &single_int_2[0] << ";" << endl;
+    cout << "&single_int_1 = " << single_int_1 << "; " << endl;
+    cout << "&single_int_2 = " << single_int_2 << ";" << endl;
 
-    delete [] single_int_1;
-    delete [] single_int_2;
+    delete single_int_1;
+    delete single_int_2;
 
     return 0;
-
 }
