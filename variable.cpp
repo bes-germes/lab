3477a575a739460477952c45bdbfde4d961b80cf
bes-germes <55259601+bes-germes@users.noreply.github.com>
2019-12-23 13:34:24 +0300

    Update variable.cpp

--- variable.cpp
+++ variable.cpp
@@ -12,8 +12,13 @@ int main()
     int local_variable_l;
     int local_variable_2;
 
-    int *single_int_1 = new int;
-    int *single_int_2 = new int;
+    int *single_int_1 = new int[size];
+    int *single_int_2 = new int[size];
+    for(int i = 0; i < size; i ++)
+    {
+        single_int_1[i] = 1;
+        single_int_2[i] = 1;
+    }
 
     cout << "&global_variable_1 = " << &global_variable_1 << "; " << endl;
     cout << "&global_variable_2 = " << &global_variable_2 << ";" << endl;
@@ -22,8 +27,8 @@ int main()
     cout << "single_int_1 = " << single_int_1 << "; " << endl;
     cout << "single_int_2 = " << single_int_2 << ";" << endl;
 
-    delete single_int_1;
-    delete single_int_2;
+    delete [] single_int_1;
+    delete [] single_int_2;
 
     return 0;
 }
